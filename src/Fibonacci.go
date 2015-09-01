1239838746fd8f9ccadadfe8219db0b025ef9d6c
Vu Dinh <vdinh@redhat.com>
2015-09-01 17:54:14 -0400

    RH Go 09/01/2015
    Signed-off-by: Vu Dinh <vdinh@redhat.com>

---
+++ src/Fibonacci.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+}
+
+func fibonacci(num int) {
+  if (num < 2) {
+    fmt.Println(1)
+  }
+  var previous, current, sum int
+  for ()
+}
---
+++ src/variables.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main() {
+  var a string = "Hello"
+  fmt.Println(a)
+
+  var b, c int = 1, 2
+  fmt.Println(b, c)
+
+  var d = true
+  fmt.Println(d)
+
+  var e int
+  fmt.Println(e)
+
+  f := "tall"
+  fmt.Println(f)
+}
